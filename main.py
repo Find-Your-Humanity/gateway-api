55700c19e1f283d1b5de3abbe6fb685c90e2fecd
kjh <kimjeahyun77@gmail.com>
2025-09-04 17:25:26 +0900

    Allow all localhost ports for development

--- main.py
+++ main.py
@@ -44,14 +44,9 @@ app.add_middleware(
         "https://www.realcatcha.com",
         "https://test.realcatcha.com",
         "https://dashboard.realcatcha.com",
-        "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost",
-        "http://localhost:8080",
-        "http://127.0.0.1",
-        "http://127.0.0.1:8080",
-        "http://127.0.0.1:8081",  # 테스트용 추가
-        "https://novelike-the-draw.static.hf.space"  # 임시 추가
+        # Allow all localhost ports for development
+        "http://localhost:*",
+        "http://127.0.0.1:*"
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
