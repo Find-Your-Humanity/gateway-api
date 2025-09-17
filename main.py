a6f30543f717441f1ad7a2d849433dc1a5fc8e30
namkyu <zeroorder31@gmail.com>
2025-09-17 18:43:06 +0900

    .

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ app.add_middleware(
         "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
     allow_headers=["*"],
     expose_headers=["*"]
 )
